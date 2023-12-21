de0e64562b42d0ca14f1f0fb160863f176c938ec
moharafat <mohamedamr0001@gmail.com>
2023-12-21 21:37:16 +0200

    yo

--- my_shell.c
+++ my_shell.c
@@ -1,22 +1,32 @@
 #include "shell.h"
 int main()
 {
-    char **buffer = 0;
-    size_t *n = 0;
+    char *buffer = NULL;  
+    size_t n = 0;
     int iread;
-    FILE *stream = 0;
-
-  char *argv[] = {"/usr/bin/pwd", NULL};
-
+    pid_t child_pid;
+    int status;
     while (1)
     {
         write(1, "($) ", 4);
-        iread = getline(buffer, n, stream);
+        buffer = malloc(2000);
+        iread = getline(&buffer, &n, stdin);
         if (iread == -1)
-        {
          perror("Error:");
+        printf("BEFORE EXECUTION & FORKING");
+        child_pid = fork();
+        if (child_pid == -1)
+        {
+            perror("ERROR");
+            return (1);
         }
-        printf("BEFORE EXECUTION");
+        if (child_pid == 0)
+            printf("I am the child, father wait for me");
+        else
+        { 
+            wait(&status);
+            printf("I am the father");
+        }
         if (execve(argv[0], argv, NULL) == -1 )
         {
          perror("Error:");   
